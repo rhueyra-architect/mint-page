5364e3e89e6d752fbe1a723451688005d38b6249
Rhueyra Realm <215103194+rhueyra-architect@users.noreply.github.com>
2025-11-30 13:44:56 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 // pages/_app.js
-import React from "react";
-import { ThirdwebProvider } from "thirdweb/react";
-import { createThirdwebClient } from "thirdweb";
-import { baseSepolia } from "thirdweb/chains";
+import '../styles/globals.css';
+import React from 'react';
+import { ThirdwebProvider } from 'thirdweb/react';
+import { createThirdwebClient } from 'thirdweb';
+import { baseSepolia } from 'thirdweb/chains';
 
+// create the client using the clientId saved in env
 const client = createThirdwebClient({
-  clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID,
+  clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID || ''
 });
 
 export default function App({ Component, pageProps }) {
