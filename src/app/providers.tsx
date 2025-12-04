bbc01947c38c23387cbb10a3f5740d046f57074a
Rhueyra Realm <215103194+rhueyra-architect@users.noreply.github.com>
2025-12-04 11:48:22 +0700

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,7 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThirdwebProvider } from "thirdweb/react";
-import { Toaster } from "@/components/ui/toast";
+import { ToastProvider } from "@/components/ui/toast";
 import { client } from "@/lib/thirdwebClient";
 
 const queryClient = new QueryClient();
@@ -10,8 +10,9 @@ const queryClient = new QueryClient();
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Toaster position="bottom-center" />
+      <ToastProvider>
       <ThirdwebProvider client={client}>{children}</ThirdwebProvider>
+      </ToastProvider>
     </QueryClientProvider>
   );
 }
