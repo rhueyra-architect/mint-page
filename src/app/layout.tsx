a3bcd25f0c0c249ca81e7fbc83d3b64d3daba131
Rhueyra Realm <215103194+rhueyra-architect@users.noreply.github.com>
2025-12-06 05:10:22 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
-import { ConnectButton } from "thirdweb/react";
+import { ConnectButton } from "@thirdweb-dev/react";
 import Providers from "./providers";   
 
 const inter = Inter({ subsets: ["latin"] });
